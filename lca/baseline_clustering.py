f69ee264ae7c95d38237ed5524d67f0bd0a90bed
Ekaterina Nepovinnykh <Ekaterina.Nepovinnykh@lut.fi>
2024-07-02 10:27:08 -0400

    add baseline method

---
+++ lca/baseline_clustering.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import networkx as nx
+import logging
+
+import cluster_tools as ct
+
+
+logger = logging.getLogger('lca')
+
+
+
+def baseline_clustering(all_nodes, verifier_edges, human_reviewer, threshold):
+
+    high_threshold = 1 - threshold
+    low_threshold = threshold
+
+    clustering = {f"ct{n}":set([n]) for n in all_nodes}
+    node2cid = {n:f"ct{n}" for n in all_nodes}
+
+    def connect_nodes(n1, n2):
+        if node2cid[n1] != node2cid[n2]:
+            cluster = node2cid[n1]
+            c2nodes = clustering.pop(node2cid[n2])
+            clustering[cluster] = clustering[cluster].union(c2nodes)
+            for n in c2nodes:
+                node2cid[n] = cluster
+
+    for_review = []
+
+    for (n1, n2, s) in verifier_edges:
+        if s > high_threshold:
+            connect_nodes(n1, n2)
+        elif s > low_threshold:
+            for_review.append((n1,n2))
+    num_human = len(for_review)
+    reviews = human_reviewer(for_review)
+    for (n1, n2, s) in reviews:
+        if s:
+            connect_nodes(n1, n2)
+    return clustering, node2cid, num_human
+
+
+
+    
---
+++ lca/run_baseline.py
@@ -0,0 +1,166 @@
+import numpy as np
+from preprocess import preprocess_data
+from embeddings import Embeddings
+from curate_using_LCA import curate_using_LCA, generate_wgtr_calibration_ground_truth
+from tools import *
+import random
+import os
+from baseline_clustering import baseline_clustering
+from cluster_validator import ClusterValidator
+import ga_driver
+from init_logger import init_logger
+
+import argparse
+
+
+def get_review(node_1, node_2, df, name_key, rate=0.98):
+
+    is_similar = False
+    if df.iloc[node_1][name_key] == df.iloc[node_2][name_key]:
+        is_similar=True
+    
+    return is_similar if random.random() < rate else not is_similar
+
+def call_get_reviews(df, name_key):
+    def get_reviews(edge_nodes, get_quit=False):
+        logger = logging.getLogger('lca')
+        reviews = [(n0, n1, get_review(n0, n1, df, name_key)) for n0, n1 in edge_nodes]
+        quit_lca = False
+        if get_quit:
+            return reviews, quit_lca
+        logger.info(f'Reviews  {reviews} ')
+        return reviews
+    return get_reviews
+    # return reviews, quit_lca
+
+
+
+def save_probs_to_db(pos, neg, output_path, method='miewid'):
+    dir_name = os.path.dirname(output_path)
+    
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+    
+    data = {
+        method: {
+            "gt_positive_probs": [p for _, _, p in pos],
+            "gt_negative_probs": [p for _, _, p in neg]
+        }
+    }
+    
+    with open(output_path, 'w') as f:
+        json.dump(data, f, indent=2)
+
+    return data
+
+def call_verifier_alg(embeddings):
+    def verifier_alg(edge_nodes):
+        logger = logging.getLogger('lca')
+        scores = [embeddings.get_score(n0, n1) for n0, n1 in edge_nodes]
+        logger.info(f'Scores  {scores} ')
+        return scores
+    return verifier_alg
+
+
+
+def run(config):
+
+    # init params
+
+    lca_config = config['lca']
+    data_params = config['data']
+    lca_params = generate_ga_params(lca_config)
+    
+    embeddings, uuids = load_pickle(data_params['embedding_file'])
+
+    #create db files
+
+    db_path = os.path.join(lca_config['db_path'], config['exp_name'])
+    os.makedirs(db_path, exist_ok=True)
+
+    verifier_file =  os.path.join(db_path, "verifiers_probs.json")
+    edge_db_file =  os.path.join(db_path, "quads.json")
+    clustering_file = os.path.join(db_path, "clustering.json")
+
+
+    # preprocess data
+
+    name_keys = data_params['name_keys']
+    filter_key = '__'.join(name_keys)
+    df = preprocess_data(data_params['annotation_file'], 
+                        name_keys= name_keys,
+                        convert_names_to_ids=True, 
+                        viewpoint_list=data_params['viewpoint_list'], 
+                        n_filter_min=data_params['n_filter_min'], 
+                        n_filter_max=data_params['n_filter_max'],
+                        images_dir = data_params['images_dir'], 
+                        embedding_uuids = uuids
+                    )
+    
+    print_intersect_stats(df, individual_key=filter_key)
+
+   
+
+    # create embeddings verifier
+
+    filtered_df = df[df['uuid_x'].isin(uuids)]
+    ids = filtered_df.index.tolist()
+
+    verifier_embeddings = Embeddings(embeddings, ids)
+    verifier_edges = verifier_embeddings.get_edges()
+
+
+    # generate wgtr calibration    
+
+    num_pos_needed = lca_params['num_pos_needed']
+    num_neg_needed = lca_params['num_neg_needed']
+    
+    human_reviewer = call_get_reviews(df, filter_key)
+
+    pos, neg, quit = generate_wgtr_calibration_ground_truth(verifier_edges, human_reviewer, num_pos_needed, num_neg_needed)
+    wgtrs_calib_dict = save_probs_to_db(pos, neg, verifier_file)
+
+    
+
+    # create cluster validator
+
+    gt_clustering, gt_node2cid = generate_gt_clusters(df, filter_key)
+    cluster_validator = ClusterValidator(gt_clustering, gt_node2cid)
+    ga_driver.set_validator_functions(cluster_validator.trace_start_human, cluster_validator.trace_iter_compare_to_gt)
+
+
+    # run baseline
+
+    human_reviews = []
+    current_clustering={}
+    cluster_data = {}
+    verifier_name = lca_config['verifier_name']
+
+    # verifier_alg = call_verifier_alg(verifier_embeddings)
+    
+    clustering, node2cid, num_human = baseline_clustering(list(gt_node2cid.keys()), verifier_edges, human_reviewer, 0.1)
+    logger = logging.getLogger('lca')
+
+    cluster_validator.incremental_stats(num_human, clustering, node2cid, gt_clustering, gt_node2cid)
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Load configuration file.")
+    parser.add_argument(
+        '--config',
+        type=str,
+        default='configs/default_config.yaml',
+        help='Path to the YAML configuration file. Default: configs/default_config.yaml'
+    )
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    init_logger()
+    args = parse_args()
+    config_path = args.config
+    
+    config = get_config(config_path)
+
+    run(config)
