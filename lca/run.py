af7a911704d5e4b068dfbb2caa1164fdc30c3ad1
Kate Nepovinnykh <kwadraterry@gmail.com>
2025-07-13 14:25:16 -0400

    add hist

--- lca/run.py
+++ lca/run.py
@@ -156,9 +156,11 @@ def main(config):
             # Run separately for each viewpoint
             for viewpoint in data_params['viewpoint_list']:
                 print(f"Running for viewpoint: {viewpoint}")
+                logger.info(f"Running for viewpoint: {viewpoint}")
                 save_dir = os.path.join(db_path, viewpoint)
                 os.makedirs(save_dir, exist_ok=True)
                 run_for_viewpoints(config, [viewpoint], save_dir)
+                config['logging']['file_mode'] = 'a'
         else:
             # Run for all viewpoints together
             run(config)
